c1393b443340bcd1be96c358a5b03311a11e741f
Cynthia Inga <candyinga67@gmail.com>
2022-03-15 18:45:05 +0200

    Add some changes to my App.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { Component } from 'react';
 import Calculator from './components/calculator';
 
-const App = () => (
-  <div>
-    <Calculator />
-  </div>
-);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
+}
 
 export default App;
